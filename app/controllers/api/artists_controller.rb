f5163a16da3de2a4c8eb7ac4255a0d34fc866ee5
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-11-21 16:08:23 -0600

    Use serializer gem

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,23 +1,15 @@
 class Api::ArtistsController < ApplicationController
 
-  protect_from_forgery with: :null_exception
+  protect_from_forgery with: :reset_session
 
   def index
     @artists = Artist.all
     render json: @artists
   end
-  
+
   def create
     @artist = Artist.new(params.require(:artists).permit(:name))
     @artist.save
     render json: @artist
   end
-
-  def update
-    @artist = Artist.find(params[:id])
-    @artis = Artist.update(params.require(:artists).permit(:name))
-    render json @artist
-  end
-
-
 end
