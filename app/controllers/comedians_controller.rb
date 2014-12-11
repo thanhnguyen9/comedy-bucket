39a7d27d90b84e5a80aec9ee6f229add82b3ed3a
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-12-11 13:22:40 -0600

    Ajax request form, change default email when user forget password

--- app/controllers/comedians_controller.rb
+++ app/controllers/comedians_controller.rb
@@ -2,11 +2,7 @@ class ComediansController < ApplicationController
 
   def create
     @comedian = Comedian.new(params.require(:comedian).permit(:name))
-    if @comedian.save
-      redirect_to root_path, notice: "Successfully Added"
-    else
-      redirect_to root_path
-    end
+    @comedian.save
   end
 
 end
