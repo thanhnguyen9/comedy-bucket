68fada04c130d3c6cb032ab5e5d1b396a81c0550
thanhnguyen <thanhnguyen.ldt@gmail.com>
2014-12-13 17:11:47 -0600

    Ajax subscribe

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,13 +7,13 @@ class RelationshipsController < ApplicationController
   def create
     @relationship = Relationship.new(user_id: current_user.id, friend_id: params[:friend_id])
     @relationship.save
-    redirect_to user_path(@relationship.friend_id), notice: "Successfully Subcribe"
+    @user = @relationship.user
   end
 
   def destroy
-    @relationship = Relationship.where(:user_id => params[:user_id], :friend_id => params[:friend_id]).first
+    @relationship = Relationship.find(params[:id])
+    @user = User.find_by(id: @relationship.user_id)
     @relationship.delete
-    redirect_to recommend_path, notice: "Successfully Unsubcribe"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @products = Product.where(:user_id => @user.id)
     @videos = Video.all.page(params[:page])
+    @relationship = Relationship.find_by(user_id: current_user.id, friend_id: @user.id)
   end
 end
